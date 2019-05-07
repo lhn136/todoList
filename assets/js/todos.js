4e737ff79d66f7e22fdacdac65a54646311e4500
lhn136 <lhn136@gmail.com>
2019-05-07 02:11:48 -0700

    added colors and all functionalities

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,29 @@
-alert("connected");
+$("input[type='text']").fadeOut()
+// hover effect
+$("ul").on("mouseenter mouseleave","li",function(event){
+    $(this).toggleClass("hoverli")
+});
+$("ul").on("click","li",function (event) {
+    $(this).toggleClass("completed")
+});
+
+// Click on x-box to delete todo
+$("ul").on("click","span",function(event) {
+    console.log("spanclicked");
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });B
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        var todoText = $(this).val();
+        $(".list").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>")
+        $(this).val("");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle()
+});
